7ba71ea611ce562ae897878936adc95ad9c5eb4c
NickFoster08 <nf26742@uga.edu>
2024-10-14 14:52:42 -0400

    fixed typo lol

--- cgw_training.sh
+++ cgw_training.sh
@@ -26,5 +26,5 @@ module load NCBI-Datasets-CLI/16.4.4
 cd $OUTDIR
 
 #download datasets and unzip
-datasets download genome acension GCF_000195955.2n --include cds,genome
+datasets download genome accession GCF_000195955.2n --include cds,genome
 unzip ncbi_dowload.zip
