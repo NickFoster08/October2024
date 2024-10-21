65f5840edd723739da415587de840b95f5a23e19
NickFoster08 <nf26742@uga.edu>
2024-10-21 14:31:02 -0400

    added =

--- bactopia_from_accessions.sh
+++ bactopia_from_accessions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name-Ethiopia_Bioproject        # Job name
+#SBATCH --job-name=Ethiopia_Bioproject        # Job name
 #SBATCH --partition=batch             # Partition (queue) name
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --cpus-per-task=8             #number of cores per task
