28fdf8c1abac44863f46f7253bde72a076ff93a8
NickFoster08 <nf26742@uga.edu>
2024-10-23 17:06:48 -0400

    changed title

--- cgw_bovis_script.sh
+++ cgw_bovis_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Mor_Bovis_Bioproject        # Job name
+#SBATCH --job-name=Bovis_Project_Morrocco        # Job name
 #SBATCH --partition=batch                      # Partition (queue) name
 #SBATCH --ntasks=1                             # Run on a single CPU
 #SBATCH --cpus-per-task=4                      # Number of cores per task
