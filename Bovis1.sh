b75ed5aa30d97942ba8f49a2ce37b307c4206704
NickFoster08 <nf26742@uga.edu>
2024-10-23 12:20:19 -0400

    complete rewrite

--- Bovis1.sh
+++ Bovis1.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-#SBATCH --job-name=Mor_Bovis_Bioproject          # Job name
-#SBATCH --partition=batch                        # Partition (queue) name
-#SBATCH --ntasks=1                               # Run on a single CPU
-#SBATCH --cpus-per-task=4                        # Number of cores per task
-#SBATCH --mem=40gb                               # Job memory request
-#SBATCH --time=02-00:00:00                       # Time limit hrs:min:sec
+#SBATCH --job-name=Mor_Bovis_Bioproject        # Job name
+#SBATCH --partition=batch                      # Partition (queue) name
+#SBATCH --ntasks=1                             # Run on a single CPU
+#SBATCH --cpus-per-task=4                      # Number of cores per task
+#SBATCH --mem=40gb                             # Job memory request
+#SBATCH --time=02-00:00:00                     # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out  # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err   # Standard error log
-#SBATCH --mail-type=END,FAIL                     # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=nf26742@uga.edu              # Where to send mail
+
+#SBATCH --mail-type=END,FAIL                   # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=nf26742@uga.edu            # Where to send mail
 
 # Set output directory variable
-OUTDIR=/home/nf26742/BovMor1/fastqs 
+OUTDIR="/home/nf26742/BovMor1/fastqs"
 
 # Create the output directory if it doesn't exist
 if [ ! -d "$OUTDIR" ]; then
     mkdir -p "$OUTDIR"
 fi
 
-# Load Bactopia module
+# Load Bactopia
 module load Bactopia/3.1.0
+
+# Move to the output directory
+cd "$OUTDIR" || { echo "Failed to change directory to $OUTDIR"; exit 1; }
 
 # Tell Bactopia to prepare samples and generate FOFN
 bactopia prepare \
@@ -29,14 +33,11 @@ bactopia prepare \
     --genome-size 2800000 \
     --pe1-pattern "_R1_001" \
     --pe2-pattern "_R2_001"
-    
-# Move to the output directory
-cd "$OUTDIR" || exit
 
 # Invoke Bactopia using the generated FOFN
 bactopia \
-    --fastqs "$FOFN" \
-    --outdir "$OUTDIR" \
+    --fastqs "$OUTDIR/samples.fofn" \
+    --outdir "$OUTDIR/" \
     --max_cpus 4
 
 # Prepare summary file
