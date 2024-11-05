1d0a6e85714c8563dfb64604ea9c35ff007545c4
NickFoster08 <nf26742@uga.edu>
2024-11-05 16:57:13 -0500

    changed xml file name

--- beast.run.1.sh
+++ beast.run.1.sh
@@ -21,7 +21,7 @@ outdir="/scratch/nf26742/scratch/beast_output"
 mkdir -p $outdir
 
 # Run BEAST with 8 threads
-beast -threads 8 /lustre2/scratch/nf26742/BovMor1/fastqs/bactopia-runs/snippy-20241025-125933/core-snp.new.2xml
+beast -threads 8 /lustre2/scratch/nf26742/BovMor1/fastqs/bactopia-runs/snippy-20241025-125933/core-snp-with-end-cap.xml
 
 # Move output files to the output directory
 mv /lustre2/scratch/nf26742/BovMor1/fastqs/bactopia-runs/snippy-20241025-125933/*.log $outdir
