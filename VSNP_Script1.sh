ba3fd58b5148d212f80951c671282434508f90d8
NickFoster08 <nf26742@uga.edu>
2024-11-08 18:40:55 -0500

    rewrite

--- VSNP_Script1.sh
+++ VSNP_Script1.sh
@@ -11,8 +11,8 @@
 #SBATCH --mail-type=END,FAIL               # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=nf26742@uga.edu        # Where to send mail
 
-# Load vsnp module (if needed)
-module load vsnp
+# Load vsnp module
+module load vsnp3/3.26
 
 # Navigate to the correct directory
 cd /scratch/nf26742/BovMor1/fastqs
@@ -22,3 +22,4 @@ vsnp3_step1.py -r1 *_R1*.fastq.gz -r2 *_R2*.fastq.gz -t Mycobacterium_AF2122
 
 # Run vsnp3_step2.py
 vsnp3_step2.py -a -t Mycobacterium_AF2122
+#test
