c902b4a98bf1337a5f365d1f4e162706629395a4
NickFoster08 <nf26742@uga.edu>
2024-11-08 16:47:32 -0500

    initial

---
+++ VSNP_Script1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=VSNP_mbovis_mor        # Job name
+#SBATCH --partition=batch                 # Partition (queue) name
+#SBATCH --ntasks=1                        # Run on a single CPU
+#SBATCH --cpus-per-task=8                 # Number of cores per task
+#SBATCH --mem=40gb                        # Job memory request
+#SBATCH --time=2-00:00:00                 # Time limit (days-hours:min:sec)
+#SBATCH --output=/scratch/nf26742/scratch/log.%j.out  # Standard output log
+#SBATCH --error=/scratch/nf26742/scratch/log.%j.err   # Standard error log
+
+#SBATCH --mail-type=END,FAIL              # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=nf26742@uga.edu       # Where to send mail
+
+# Load VSNP module
+module load vsnp3/3.26
+
+# Define output directory
+OUTDIR="/home/nf26742/vsnpBovisWorkflow"  # No `$` here
+
+# Navigate to working directory with fastqs
+cd /scratch/nf26742/BovMor1/fastqs
+
+# Run vSNP command
+vsnp3_step1.py -o $OUTDIR
