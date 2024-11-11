ca2bec10ea0fc1171d82564c2e07e33a4433ea6b
NickFoster08 <nf26742@uga.edu>
2024-11-11 13:34:28 -0500

    rewrite for one file

--- VSNP_Script1.sh
+++ VSNP_Script1.sh
@@ -18,7 +18,6 @@ module load vsnp3/3.26
 cd /scratch/nf26742/BovMor1/fastqs
 
 # Run vsnp3_step1.py with the appropriate file patterns
-vsnp3_step1.py \
-    -r1 /scratch/nf26742/BovMor1/fastqs/*_R1.fastq.gz \
-    -r2 /scratch/nf26742/BovMor1/fastqs/*_R2.fastq.gz \
-    -t Mycobacterium_AF2122
+vsnp3_step1.py -r1 /scratch/nf26742/BovMor1/fastqs/17-12281_S22_L001_R1.fastq.gz \
+               -r2 /scratch/nf26742/BovMor1/fastqs/17-12281_S22_L001_R2.fastq.gz \
+               -t Mycobacterium_AF2122
