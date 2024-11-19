c3272b984a44fd6117f11341bc4ea4216111dd15
NickFoster08 <nf26742@uga.edu>
2024-11-19 17:30:47 -0500

    added annotations

--- VSNP_LOOP_2.sh
+++ VSNP_LOOP_2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=VSNP_mbovis_Loop        # Job name
-#SBATCH --partition=batch                     # Partition (queue) name
-#SBATCH --ntasks=1                            # Run on a single CPU
-#SBATCH --cpus-per-task=8                     # Number of cores per task
-#SBATCH --mem=40gb                            # Job memory request
-#SBATCH --time=02-00:00:00                    # Time limit hrs:min:sec
+#SBATCH --partition=batch                 # Partition (queue) name
+#SBATCH --ntasks=1                        # Run on a single CPU
+#SBATCH --cpus-per-task=8                 # Number of cores per task
+#SBATCH --mem=40gb                        # Job memory request
+#SBATCH --time=02-00:00:00                # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out  # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err   # Standard error log
 
-#SBATCH --mail-type=END,FAIL                  # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=nf26742@uga.edu           # Where to send mail
+#SBATCH --mail-type=END,FAIL              # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=nf26742@uga.edu       # Where to send mail
 
 # Set output directory variable
 OUTDIR=/scratch/nf26742/BovMor1/fastqs/VSNP_Output
@@ -28,6 +28,9 @@ cd /scratch/nf26742/BovMor1/fastqs
 # Reference genome file
 REFERENCE="/scratch/nf26742/BovMor1/fastqs/GCF_000195955.2_ASM19595v2_genomic.fna"
 
+# Annotation file
+ANNOTATION="/scratch/nf26742/BovMor1/fastqs/genomic.gff"
+
 # Loop through all R1 fastq files and find their corresponding R2
 for R1 in *_R1.fastq.gz; do
     SAMPLE=$(basename $R1 _R1.fastq.gz)
@@ -41,6 +44,7 @@ for R1 in *_R1.fastq.gz; do
             -r1 "$R1" \
             -r2 "$R2" \
             -r "$REFERENCE" \
+            -a "$ANNOTATION" \
             -o "$OUTDIR/$SAMPLE"
 
     else
