bdf03c6d894c5e8f934ef3bd26caa6aa3081d140
NickFoster08 <nf26742@uga.edu>
2024-11-19 16:02:55 -0500

    re write

--- SNP_Gene_Name.sh
+++ SNP_Gene_Name.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=Replace_Coord_Gene_Name      # Job name
+#SBATCH --job-name=Replace_COord_Gene_Name      # Job name
 #SBATCH --partition=batch                     # Partition (queue) name
 #SBATCH --ntasks=1                            # Run on a single CPU
 #SBATCH --cpus-per-task=8                     # Number of cores per task
 #SBATCH --mem=40gb                            # Job memory request
-#SBATCH --time=02-00:00:00                    # Time limit hrs:min:sec
+#SBATCH --time= 20:00:00                    # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out  # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err   # Standard error log
 
@@ -27,7 +27,10 @@ python - <<EOF
 import pandas as pd
 
 # Load the SNP Excel file (adjust the filename to your actual file)
-snp_df = pd.read_excel('/scratch/nf26742/BovMor1/fastqs/VCF_Files/all_vcf/all_vcf_sorted_table-2024-11-18_16-27-19.xlsx')
+snp_df = pd.read_excel(
+    '/scratch/nf26742/BovMor1/fastqs/VCF_Files/all_vcf/all_vcf_sorted_table-2024-11-18_16-27-19.xlsx',
+    engine='openpyxl'
+)
 
 # Load the GFF3 file for gene annotations
 gff3_file = '/scratch/nf26742/BovMor1/fastqs/sequence.gff3'
