3f07dbaa13edd4d89073de8b90e819b8f8bc9d1d
NickFoster08 <nf26742@uga.edu>
2024-11-20 20:36:02 -0500

    rewrite for gbff.gz

--- conversion_full.sh
+++ conversion_full.sh
@@ -12,7 +12,7 @@
 #SBATCH --mail-user=nf26742@uga.edu              # Where to send mail
 
 #set output directory variable
-OUTDIR=/scratch/nf26742/BovMor1/fastqs/Ref_Mbov/GCF_000195835.3_ASM19583v2_genomic.gbff.gz
+OUTDIR=/scratch/nf26742/BovMor1/fastqs/Ref_Mbov
 SCRIPT=/home/nf26742/October2024
 
 #load modules
@@ -21,4 +21,14 @@ module load Biopython/1.84-foss-2023b
 #move to working directory
 cd $OUTDIR
 
-python $SCRIPT/conversion_gbf_gbk.py $OUTDIR/genomic.gbff $OUTDIR/genomic.gbk
+# Decompress the .gz file (if needed)
+gunzip GCF_000195835.3_ASM19583v2_genomic.gbff.gz
+
+# Log the start of the process
+echo "Starting conversion..." > $OUTDIR/conversion.log
+
+# Run the Python script for conversion
+python $SCRIPT/conversion_gbf_gbk.py $OUTDIR/GCF_000195835.3_ASM19583v2_genomic.gbff $OUTDIR/genomic.gbk >> $OUTDIR/conversion.log 2>&1
+
+# Log completion
+echo "Conversion completed." >> $OUTDIR/conversion.log
