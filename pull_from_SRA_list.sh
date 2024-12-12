b50e03b462b69c85ae09bd2bc704cf279688deef
NickFoster08 <nf26742@uga.edu>
2024-12-12 16:20:41 -0500

    typo

--- pull_from_SRA_list.sh
+++ pull_from_SRA_list.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=spain downlaod       # Job name
+#SBATCH --job-name=spain_downlaod       # Job name
 #SBATCH --partition=batch                       # Partition (queue) name
 #SBATCH --ntasks=1                              # Run on a single CPU
 #SBATCH --cpus-per-task=2                       # Number of cores per task
