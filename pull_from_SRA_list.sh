28a031be864d92cb7bb5ab44f9e89bb5794318c3
NickFoster08 <nf26742@uga.edu>
2024-12-20 16:32:17 -0500

    added debugging

--- pull_from_SRA_list.sh
+++ pull_from_SRA_list.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=portugal_downlaod_AF_2015      # Job name
+#SBATCH --job-name=portugal_download_AF_2015      # Job name
 #SBATCH --partition=batch               # Partition (queue) name
 #SBATCH --ntasks=1                      # Run on a single CPU
 #SBATCH --cpus-per-task=8               # Number of cores per task
@@ -12,11 +12,13 @@
 #SBATCH --mail-user=nf26742@uga.edu     # Where to send mail
 
 OUTDIR="/home/nf26742/All_Seqs/Portugal/AF_2015"
+SRA_DIR="/home/nf26742/sra_files"   # Directory for storing .sra files
 
 module load SRA-Toolkit/3.0.3-gompi-2022a  
 
 # Ensure the output directory exists
 mkdir -p $OUTDIR
+mkdir -p $SRA_DIR
 
 # List of accessions
 accessions=(
@@ -34,8 +36,8 @@ SAMN33871459
 
 # Loop to download and convert SRA files
 for accession in "${accessions[@]}"; do
-    # Download the SRA file
-    prefetch $accession
-    # Convert to FASTQ format
-    fasterq-dump $accession -O $OUTDIR
+    # Download the SRA file and specify the output directory
+    prefetch --output-directory $SRA_DIR $accession --verbose
+    # Convert to FASTQ format and specify both input and output directories
+    fasterq-dump $SRA_DIR/$accession.sra --verbose -O $OUTDIR
 done
