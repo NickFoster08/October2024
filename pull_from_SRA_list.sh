dc465eb47aa578a8e10f449785c6483a9e4c8679
NickFoster08 <nf26742@uga.edu>
2024-12-20 18:39:54 -0500

    rewrtie

--- pull_from_SRA_list.sh
+++ pull_from_SRA_list.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-#SBATCH --job-name=portugal_download_AF_2015      # Job name
-#SBATCH --partition=batch               # Partition (queue) name
-#SBATCH --ntasks=1                      # Run on a single CPU
-#SBATCH --cpus-per-task=8               # Number of cores per task
-#SBATCH --mem=75gb                      # Job memory request
-#SBATCH --time=00-12:00:00              # Time limit hrs:min:sec
+#SBATCH --job-name=spain_downlaod       # Job name
+#SBATCH --partition=batch                       # Partition (queue) name
+#SBATCH --ntasks=1                              # Run on a single CPU
+#SBATCH --cpus-per-task=8                       # Number of cores per task
+#SBATCH --mem=80gb                              # Job memory request
+#SBATCH --time=00-012:00:00                      # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out  # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err   # Standard error log
 
-#SBATCH --mail-type=END,FAIL            # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=nf26742@uga.edu     # Where to send mail
+#SBATCH --mail-type=END,FAIL                    # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=nf26742@uga.edu             # Where to send mail
 
+#specify outdir
 OUTDIR="/home/nf26742/All_Seqs/Portugal/AF_2015"
-SRA_DIR="/home/nf26742/sra_files"   # Directory for storing .sra files
 
-module load SRA-Toolkit/3.0.3-gompi-2022a  
-
-# Ensure the output directory exists
-mkdir -p $OUTDIR
-mkdir -p $SRA_DIR
+#load SRA tools
+module load SRA-Toolkit/3.0.3-gompi-2022a
 
 # List of accessions
 accessions=(
+SAMN33843519
 SAMN33843536
 SAMN33843537
 SAMN33843539
@@ -36,8 +34,8 @@ SAMN33871459
 
 # Loop to download and convert SRA files
 for accession in "${accessions[@]}"; do
-    # Download the SRA file and specify the output directory
-    prefetch --output-directory $SRA_DIR $accession --verbose
-    # Convert to FASTQ format and specify both input and output directories
-    fasterq-dump $SRA_DIR/$accession.sra --verbose -O $OUTDIR
+    # Download the SRA file
+    prefetch $accession
+    # Convert to FASTQ format
+    fasterq-dump $accession -O /$OUTDIR
 done
