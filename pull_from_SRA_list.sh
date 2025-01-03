d00b767adf2fb9ea381b8e1c3ed0543b1f8f6264
NickFoster08 <nf26742@uga.edu>
2025-01-03 17:04:53 -0500

    fixed minor typos from GPT

--- pull_from_SRA_list.sh
+++ pull_from_SRA_list.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=ptgal_downlaod       # Job name
-#SBATCH --partition=batch                       # Partition (queue) name
-#SBATCH --ntasks=1                              # Run on a single CPU
-#SBATCH --cpus-per-task=8                       # Number of cores per task
-#SBATCH --mem=80gb                              # Job memory request
-#SBATCH --time=00-012:00:00                      # Time limit hrs:min:sec
+#SBATCH --job-name=ptgal_download       # Job name
+#SBATCH --partition=batch              # Partition (queue) name
+#SBATCH --ntasks=1                     # Run on a single CPU
+#SBATCH --cpus-per-task=8              # Number of cores per task
+#SBATCH --mem=80gb                     # Job memory request
+#SBATCH --time=00-12:00:00             # Time limit hrs:min:sec
 #SBATCH --output=/scratch/nf26742/scratch/log.%j.out  # Standard output log
 #SBATCH --error=/scratch/nf26742/scratch/log.%j.err   # Standard error log
 
-#SBATCH --mail-type=END,FAIL                    # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=nf26742@uga.edu             # Where to send mail
+#SBATCH --mail-type=END,FAIL           # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=nf26742@uga.edu    # Where to send mail
 
-#specify outdir
+# Specify output directory
 OUTDIR="/home/nf26742/All_Seqs/Portugal/BF_2015"
 
-#load SRA tools
+# Load SRA tools
 module load SRA-Toolkit/3.0.3-gompi-2022a
+
+# Ensure output directory exists
+mkdir -p "$OUTDIR"
 
 # List of accessions
 accessions=(
@@ -57,7 +60,7 @@ SAMN17004141
 # Loop to download and convert SRA files
 for accession in "${accessions[@]}"; do
     # Download the SRA file
-    prefetch $accession
+    prefetch "$accession"
     # Convert to FASTQ format
-    fasterq-dump $accession -O $OUTDIR
+    fasterq-dump "$accession" -O "$OUTDIR"
 done
