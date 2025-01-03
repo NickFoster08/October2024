95d3da35f39af892b156232863b11b44fd548036
NickFoster08 <nf26742@uga.edu>
2025-01-03 14:27:43 -0500

    portugal update b4 2015

--- pull_from_SRA_list.sh
+++ pull_from_SRA_list.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=spain_downlaod       # Job name
+#SBATCH --job-name=ptgal_downlaod       # Job name
 #SBATCH --partition=batch                       # Partition (queue) name
 #SBATCH --ntasks=1                              # Run on a single CPU
 #SBATCH --cpus-per-task=8                       # Number of cores per task
@@ -12,24 +12,46 @@
 #SBATCH --mail-user=nf26742@uga.edu             # Where to send mail
 
 #specify outdir
-OUTDIR="/home/nf26742/All_Seqs/Portugal/AF_2015"
+OUTDIR="/home/nf26742/All_Seqs/Portugal/BF_2015"
 
 #load SRA tools
 module load SRA-Toolkit/3.0.3-gompi-2022a
 
 # List of accessions
 accessions=(
-SAMN33843519
-SAMN33843536
-SAMN33843537
-SAMN33843539
-SAMN33843555
-SAMN33843557
-SAMN33843570
-SAMN33843572
-SAMN33849789
-SAMN33871440
-SAMN33871459
+SAMN33843487
+SAMN33843486
+SAMN33843481
+SAMN33843468
+SAMN33843467
+SAMN33843466
+SAMN33843461
+SAMN33843460
+SAMN33843459
+SAMN33843455
+SAMN33843447
+SAMN33843446
+SAMN33843445
+SAMN33843443
+SAMN33843442
+SAMN33843441
+SAMN33843440
+SAMN17004175
+SAMN17004173
+SAMN17004170
+SAMN17004165
+SAMN17004164
+SAMN17004161
+SAMN17004158
+SAMN17004154
+SAMN17004153
+SAMN17004151
+SAMN17004150
+SAMN17004147
+SAMN17004145
+SAMN17004144
+SAMN17004143
+SAMN17004141
 )
 
 # Loop to download and convert SRA files
