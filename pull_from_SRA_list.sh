e10b74c3e1dc5cbca807d73ccfd01f0bf902b7f8
NickFoster08 <nf26742@uga.edu>
2025-02-06 16:24:26 -0500

    rewrite for all country files

--- pull_from_SRA_list.sh
+++ pull_from_SRA_list.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=algeria_AF2015      # Job name
+#SBATCH --job-name=tunisia_BF2015      # Job name
 #SBATCH --partition=batch                # Partition (queue) name
 #SBATCH --ntasks=1                       # Run on a single CPU
 #SBATCH --cpus-per-task=8                # Number of cores per task
@@ -12,7 +12,7 @@
 #SBATCH --mail-user=nf26742@uga.edu      # Where to send mail
 
 # Specify output directory
-OUTDIR="/home/nf26742/All_Seqs/Algeria/AF_2015"
+OUTDIR="/home/nf26742/All_Seqs/Tunisia/BF_2015"
 
 # Create output directory if it doesn't exist
 mkdir -p "$OUTDIR"
@@ -22,32 +22,6 @@ module load SRA-Toolkit/3.0.3-gompi-2022a
 
 # List of accessions
 accessions=(
-SAMEA6617092
-SAMEA6617091
-SAMEA6617080
-SAMEA6617081
-SAMEA6617082
-SAMEA6617083
-SAMEA6617084
-SAMEA6617085
-SAMEA6617086
-SAMEA6617087
-SAMEA6617089
-SAMEA6617088
-SAMEA6617090
-SRR13986556
-SRR13986557
-SRR13986558
-SRR13986559
-SRR13986560
-SRR13986561
-SRR13986562
-SRR13986563
-SRR13986564
-SRR13986565
-SRR13986566
-SRR13986567
-SRR13986568
 )
 
 # Loop to download and convert SRA files
--- vSNP_Optimized.sh
+++ vSNP_Optimized.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=vSNP_Optimized        # Job name
+#SBATCH --job-name=vSNP_AllSeqs        # Job name
 #SBATCH --partition=batch                 # Partition (queue) name
 #SBATCH --ntasks=1                        # Run on a single CPU
 #SBATCH --cpus-per-task=8                 # Number of cores per task
@@ -12,7 +12,7 @@
 #SBATCH --mail-user=nf26742@uga.edu       # Where to send mail
 
 # Set output directory variable
-OUTDIR=/scratch/nf26742/BovMor1/fastqs/VSNP_Output
+OUTDIR=/scratch/nf26742/AllSeqsAttempt1
 
 # Create the output directory if it doesn't exist
 if [ ! -d "$OUTDIR" ]; then
@@ -27,12 +27,17 @@ REFERENCE="/home/nf26742/vsnp3_test_dataset/vsnp_dependencies/Mycobacterium_AF21
 module load vsnp3/3.26
 
 # Navigate to the correct directory
-cd /scratch/nf26742/BovMor1/fastqs
+cd /home/nf26742/All_Seqs
 
-# Run vsnp3_step1.py for each pair of R1 and R2 files
-vsnp3_step1.py \
-    -r1 "*_L001_R1.fastq.gz" \
-    -r2 "*_L001_R2.fastq.gz" \
-    -t "$REFERENCE" \
-    -o "$OUTDIR"
+# Find all R1 and R2 files recursively and group them by sample
+find /home/nf26742/All_Seqs -type f -name "*_1.fastq" | while read R1; do
+    R2="${R1%_1.fastq}_2.fastq"  # Construct the corresponding R2 filename
+    
+    if [ -f "$R2" ]; then  # Ensure R2 exists
+        echo "Processing: $R1 and $R2"
+        vsnp3_step1.py -r1 "$R1" -r2 "$R2" -t "$REFERENCE" -o "$OUTDIR"
+    else
+        echo "Warning: Matching R2 file for $R1 not found!" >&2
+    fi
+done
 
